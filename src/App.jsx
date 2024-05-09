a4e300868da07971fc38a870584571b32de310a0
tmachine <tmachine@sys04.PEC.IN>
2024-05-09 13:12:22 +0530

    login started

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
+import Login from  './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <h1>HELLO</h1>
+      <Login/>
     </div>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import logo from "../assets/foodlogo.png";
 
-export default function Login() {
+const Login = () => {
   return (
-    <div>
+    <div className="bg-[#4D989D] w-full text-white flex justify-between items-center px-4 py-2 mt-4 ml-4"> {/* Added margin top (mt-4) and left (ml-4) */}
+      <div className="flex items-center">
+        <img src={logo} alt="Logo" className="w-6 h-6 object-contain" /> {/* Reduced logo size to w-6 h-6 */}
+        
+      </div>
+      <div>
+    </div>
+       
       
     </div>
-  )
-}
+  );
+};
+
+export default Login;
